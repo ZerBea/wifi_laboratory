cc2de9d54d33b7853968c547de47a72251e432ab
ZeroBeat <ZeroBeat@gmx.de>
2022-03-11 12:53:11 +0100

    reverted last commit, because it doesn't make sense if PMKID caching is disabled

--- hcxlabtool.c
+++ hcxlabtool.c
@@ -2165,14 +2165,14 @@ for(p = 0; p < BSSIDLIST_MAX; p++)
 		if((bssidlist +p)->bssidinfo->deauthattackcount == ((bssidlist +p)->bssidinfo->deauthattackfactor +6))
 			{
 			if(memcmp(&mac_broadcast, (bssidlist +p)->bssidinfo->macclient, 6) == 0) send_pspoll(p);
-			else if(((bssidlist +p)->bssidinfo->status &BSSID_M1) == 0) send_authentication_req_opensystem_cl(p);
+			else send_authentication_req_opensystem_cl(p);
 			return;
 			}
 		if((bssidlist +p)->bssidinfo->deauthattackcount == ((bssidlist +p)->bssidinfo->deauthattackfactor +10))
 			{
 			if(memcmp(&mac_broadcast, (bssidlist +p)->bssidinfo->macclient, 6) == 0) send_pspoll(p);
 			else if(((bssidlist +p)->bssidinfo->essidlen == 0) || ((bssidlist +p)->bssidinfo->essid[0] == 0)) send_pspoll(p);
-			else if(((bssidlist +p)->bssidinfo->status &BSSID_M1) == 0)
+			else
 				{
 				if((((bssidlist +p)->bssidinfo->rsnakm &TAK_PSK) == TAK_PSK)) send_association_req_wpa2_cl(p);
 				else if((((bssidlist +p)->bssidinfo->wpaakm &TAK_PSK) == TAK_PSK)) send_association_req_wpa1_cl(p);
